910d21060c76ac4f2341629db070104b157944f6
yjs5060 <142709094+yjs5060@users.noreply.github.com>
2025-05-05 01:17:47 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,28 @@
+const colors = [
+    '#ef5777',
+    '#575fcf',
+    '#4bcffa',
+    '#34e7e4',
+    '#0be881',
+    '#f53b57',
+    '#3c40c6',
+    '#0fbcf9',
+    '#00d8d6',
+    '#05c46b',
+    '#ffc048',
+    '#ffdd59',
+    '#ff5e57',
+    '#d2dae2',
+    '#485460',
+    '#ffa801',
+    '#ffd32a',
+    '#ff3f34',
+  ];
+  
+  function changeBackground() {
+    const color1 = colors[Math.floor(Math.random() * colors.length)];
+    const color2 = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+  }
+  
+  changeBackground();
